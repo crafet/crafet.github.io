c8b84f7a4861dd1de1959076c281d029c01375ad
crafet <yilinliu@tencent.com>
2018-05-15 19:26:43 +0800

    :bug: copy theme manully, shit

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ git init # 初始化一个 Repo
 git config --global push.default matching
 git config --global user.email "${GitHubEMail}"
 git config --global user.name "${GitHubUser}" # 利用在环境变量中定义的信息配置 Git
+cp  ../themes/ ./ -r
 git add --all .
 git commit -m "Auto Builder of ${GitHubUser}'s Blog" # commit 信息
-cp  ../themes/ ./ -r
+
 git push --quiet -f https://${GitHubKEY}@github.com/${GitHubUser}/${GitHubRepo}.git master # 将生成的静态整站部署到指定 Repo 的 master 分支。
